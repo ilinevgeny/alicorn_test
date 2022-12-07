66a6601a0f0ec1a2256287297da74107c452de86
Ev.Il <xzenya@gmail.com>
2022-12-07 17:02:38 +0200

    fix code style

--- src/PizzaCatalog.php
+++ src/PizzaCatalog.php
@@ -132,36 +132,3 @@ class PizzaCatalog implements PizzaCatalogInterface
         return $this->collectionPizza[$name];
     }
 }
-
-//$ingredientCatalog = new IngredientCatalog();
-//$ingredientCatalog->addIngredient(new Ingredient('tomato', 0.5));
-//$ingredientCatalog->addIngredient(new Ingredient('sliced mushrooms', 0.5));
-//$ingredientCatalog->addIngredient(new Ingredient('feta cheese', 1));
-//$ingredientCatalog->addIngredient(new Ingredient('sausages', 1));
-//$ingredientCatalog->addIngredient(new Ingredient('mozzarella cheese', 1.3));
-//
-//$catalog = new PizzaCatalog();
-//$catalog->addPizza(new Pizza('MacDac Pizza', $ingredientCatalog->getIngredients(array('tomato', 'sausages', 'feta cheese'))));
-//$catalog->addPizza(new Pizza('Lovely Mushroom Pizza', $ingredientCatalog->getIngredients(array('tomato', 'sausages'))));
-//
-//$pizza = $catalog->getPizza('MacDac Pizza');
-//echo $pizza->getPrice() . PHP_EOL;
-//
-//$pizza->addIngredient($ingredientCatalog->getIngredient('sliced mushrooms'));
-//
-//echo $pizza->getPrice() . PHP_EOL;
-//$pizza->removeIngredient('tomato');
-//$pizza->removeIngredient('feta cheese');
-//$pizza->ingredientOnTop('sliced mushrooms');
-//
-//$pizza->ingredientOnTop('tomato');
-//$pizza->ingredientOnTop('sausages');
-//
-//echo $pizza->getIngredientOrder() . PHP_EOL;
-//$pizza->ingredientOnTop('sliced mushrooms');
-//$pizza->addIngredient($ingredientCatalog->getIngredient('sliced mushrooms'));
-//echo $pizza->getIngredientOrder() . PHP_EOL;
-//$pizza->addIngredient($ingredientCatalog->getIngredient('mozzarella cheese'));
-//$pizza->ingredientOnTop('mozzarella cheese');
-//echo $pizza->getIngredientOrder() . PHP_EOL;
-//echo $pizza->getPrice();
